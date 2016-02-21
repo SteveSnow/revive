08c1f2573a58bd1ea1bf5152f49d51fca98e5cfc
SteveSnow <Steven.Snow@outlook.com>
2016-02-21 11:12:42 -0500

    added new js file

---
+++ public/scripts/faq.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function() {
+    $('.question').click(function(e) {
+        changeOpen(e)
+    });
+    closeAnswers();
+});
+
+
+
+// to allow multiple questions to be opened comment out the closeAnswers()
+// call in the if statement below.
+// we can also poentially animate the opening and closings to make them smoother
+function changeOpen(e) {
+    var q = e.target.id
+    var a = q.replace('q', 'a')
+    var aobj = $('#' + a)
+
+    if (aobj.is(":visible") === false) {
+        closeAnswers()
+        aobj.show()
+    } else {
+        aobj.hide()
+    }
+}
+
+
+function closeAnswers() {
+    var a = $('.answer');
+    for (var i = 0; i < a.length; i++) {
+        $('#' + a[i].id).hide()
+    }
+
+}
